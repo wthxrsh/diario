db460184a2df86917f0a1c762cc019dce5f91ab6
wthxrsh <wthxrsh@kali.wthxrsh>
2025-06-24 09:28:08 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from 'path'
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -11,11 +12,14 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // middleware
-app.use(cors({
+if(process.env.NODE_ENV !== "production"){
+  app.use(cors({
   origin:"http://localhost:5173",
 }))
+}
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
 
 app.use(rateLimiter);
@@ -29,6 +33,13 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get("*", (req, res)=>{
+  res.sendFile(path.join(__dirname,"../frontend/","dist","index.html"));
+});
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
