bc9bb40f49d0c203b6e2bca9b94dd85b9ec5c2b9
wthxrsh <wthxrsh@kali.wthxrsh>
2025-06-24 12:19:05 +0530

    redone some stuff again and again

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,5 @@ import axios from "axios";
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 const api = axios.create({ 
     baseURL : BASE_URL,
- })
-
- export default api;
+ });
+export default api;
